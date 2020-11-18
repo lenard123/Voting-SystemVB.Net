ed99019f2adfb75071d12d65a5318e2adfd78d61
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-11-18 16:06:11 +0800

    Initial Commit

---
+++ Voting-SystemVB/Main.vb
@@ -0,0 +1,13 @@
+﻿Public Class Main
+
+    Public Shared Instance As Main
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Instance = Me
+    End Sub
+
+End Class
---
+++ Voting-SystemVB/VoterLogin.vb
@@ -0,0 +1,3 @@
+﻿Public Class VoterLogin
+
+End Class
