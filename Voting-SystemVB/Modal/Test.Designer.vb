8ad6f9cfab6b893f11e3d884e1eddafe2a14a37e
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-11-30 02:19:06 +0800

    Fix ModalPopup

--- Voting-SystemVB/Modal/Test.Designer.vb
+++ Voting-SystemVB/Modal/Test.Designer.vb
@@ -22,19 +22,200 @@ Partial Class Test
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Me.Guna2AnimateWindow1 = New Guna.UI2.WinForms.Guna2AnimateWindow(Me.components)
+        Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2Separator1 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.TextStudentID = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.TextFirstname = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.TextLastname = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2HtmlLabel2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel3 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel4 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Button2 = New Guna.UI2.WinForms.Guna2Button()
         Me.SuspendLayout()
+        '
+        'Guna2HtmlLabel1
+        '
+        Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(47, 21)
+        Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
+        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(156, 63)
+        Me.Guna2HtmlLabel1.TabIndex = 6
+        Me.Guna2HtmlLabel1.Text = "<h1>Register Voter</h1>"
+        '
+        'Guna2Separator1
+        '
+        Me.Guna2Separator1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Guna2Separator1.Location = New System.Drawing.Point(47, 90)
+        Me.Guna2Separator1.Name = "Guna2Separator1"
+        Me.Guna2Separator1.Size = New System.Drawing.Size(477, 10)
+        Me.Guna2Separator1.TabIndex = 8
+        '
+        'TextStudentID
+        '
+        Me.TextStudentID.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextStudentID.BorderRadius = 5
+        Me.TextStudentID.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextStudentID.DefaultText = ""
+        Me.TextStudentID.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.TextStudentID.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.TextStudentID.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextStudentID.DisabledState.Parent = Me.TextStudentID
+        Me.TextStudentID.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextStudentID.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextStudentID.FocusedState.Parent = Me.TextStudentID
+        Me.TextStudentID.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextStudentID.HoverState.Parent = Me.TextStudentID
+        Me.TextStudentID.Location = New System.Drawing.Point(47, 158)
+        Me.TextStudentID.Name = "TextStudentID"
+        Me.TextStudentID.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.TextStudentID.PlaceholderText = "(e.g. 20200000-C)"
+        Me.TextStudentID.SelectedText = ""
+        Me.TextStudentID.ShadowDecoration.Parent = Me.TextStudentID
+        Me.TextStudentID.Size = New System.Drawing.Size(477, 36)
+        Me.TextStudentID.TabIndex = 7
+        '
+        'TextFirstname
+        '
+        Me.TextFirstname.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextFirstname.BorderRadius = 5
+        Me.TextFirstname.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextFirstname.DefaultText = ""
+        Me.TextFirstname.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.TextFirstname.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.TextFirstname.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextFirstname.DisabledState.Parent = Me.TextFirstname
+        Me.TextFirstname.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextFirstname.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextFirstname.FocusedState.Parent = Me.TextFirstname
+        Me.TextFirstname.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextFirstname.HoverState.Parent = Me.TextFirstname
+        Me.TextFirstname.Location = New System.Drawing.Point(47, 252)
+        Me.TextFirstname.Name = "TextFirstname"
+        Me.TextFirstname.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.TextFirstname.PlaceholderText = "(e.g. John)"
+        Me.TextFirstname.SelectedText = ""
+        Me.TextFirstname.ShadowDecoration.Parent = Me.TextFirstname
+        Me.TextFirstname.Size = New System.Drawing.Size(477, 36)
+        Me.TextFirstname.TabIndex = 9
+        '
+        'TextLastname
+        '
+        Me.TextLastname.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextLastname.BorderRadius = 5
+        Me.TextLastname.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextLastname.DefaultText = ""
+        Me.TextLastname.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.TextLastname.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.TextLastname.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextLastname.DisabledState.Parent = Me.TextLastname
+        Me.TextLastname.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.TextLastname.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextLastname.FocusedState.Parent = Me.TextLastname
+        Me.TextLastname.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextLastname.HoverState.Parent = Me.TextLastname
+        Me.TextLastname.Location = New System.Drawing.Point(47, 350)
+        Me.TextLastname.Name = "TextLastname"
+        Me.TextLastname.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.TextLastname.PlaceholderText = "(e.g. John)"
+        Me.TextLastname.SelectedText = ""
+        Me.TextLastname.ShadowDecoration.Parent = Me.TextLastname
+        Me.TextLastname.Size = New System.Drawing.Size(477, 36)
+        Me.TextLastname.TabIndex = 10
+        '
+        'Guna2HtmlLabel2
+        '
+        Me.Guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel2.Font = New System.Drawing.Font("Segoe UI", 14.0!)
+        Me.Guna2HtmlLabel2.Location = New System.Drawing.Point(47, 125)
+        Me.Guna2HtmlLabel2.Name = "Guna2HtmlLabel2"
+        Me.Guna2HtmlLabel2.Size = New System.Drawing.Size(99, 27)
+        Me.Guna2HtmlLabel2.TabIndex = 11
+        Me.Guna2HtmlLabel2.Text = "Student ID<font color=""red"">*</font>"
+        '
+        'Guna2HtmlLabel3
+        '
+        Me.Guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel3.Font = New System.Drawing.Font("Segoe UI", 14.0!)
+        Me.Guna2HtmlLabel3.Location = New System.Drawing.Point(47, 223)
+        Me.Guna2HtmlLabel3.Name = "Guna2HtmlLabel3"
+        Me.Guna2HtmlLabel3.Size = New System.Drawing.Size(93, 27)
+        Me.Guna2HtmlLabel3.TabIndex = 12
+        Me.Guna2HtmlLabel3.Text = "Firstname<font color=""red"">*</font>"
+        '
+        'Guna2HtmlLabel4
+        '
+        Me.Guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel4.Font = New System.Drawing.Font("Segoe UI", 14.0!)
+        Me.Guna2HtmlLabel4.Location = New System.Drawing.Point(47, 321)
+        Me.Guna2HtmlLabel4.Name = "Guna2HtmlLabel4"
+        Me.Guna2HtmlLabel4.Size = New System.Drawing.Size(91, 27)
+        Me.Guna2HtmlLabel4.TabIndex = 13
+        Me.Guna2HtmlLabel4.Text = "Lastname<font color=""red"">*</font>"
+        '
+        'Guna2Button1
+        '
+        Me.Guna2Button1.CheckedState.Parent = Me.Guna2Button1
+        Me.Guna2Button1.CustomImages.Parent = Me.Guna2Button1
+        Me.Guna2Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2Button1.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button1.HoverState.Parent = Me.Guna2Button1
+        Me.Guna2Button1.Location = New System.Drawing.Point(559, 90)
+        Me.Guna2Button1.Name = "Guna2Button1"
+        Me.Guna2Button1.ShadowDecoration.Parent = Me.Guna2Button1
+        Me.Guna2Button1.Size = New System.Drawing.Size(180, 45)
+        Me.Guna2Button1.TabIndex = 14
+        Me.Guna2Button1.Text = "Submit"
+        '
+        'Guna2Button2
+        '
+        Me.Guna2Button2.BorderThickness = 1
+        Me.Guna2Button2.CheckedState.Parent = Me.Guna2Button2
+        Me.Guna2Button2.CustomImages.Parent = Me.Guna2Button2
+        Me.Guna2Button2.FillColor = System.Drawing.Color.White
+        Me.Guna2Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2Button2.ForeColor = System.Drawing.Color.Black
+        Me.Guna2Button2.HoverState.Parent = Me.Guna2Button2
+        Me.Guna2Button2.Location = New System.Drawing.Point(559, 149)
+        Me.Guna2Button2.Name = "Guna2Button2"
+        Me.Guna2Button2.ShadowDecoration.Parent = Me.Guna2Button2
+        Me.Guna2Button2.Size = New System.Drawing.Size(180, 45)
+        Me.Guna2Button2.TabIndex = 14
+        Me.Guna2Button2.Text = "Add Admin"
         '
         'Test
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Guna2Button2)
+        Me.Controls.Add(Me.Guna2Button1)
+        Me.Controls.Add(Me.Guna2HtmlLabel1)
+        Me.Controls.Add(Me.Guna2Separator1)
+        Me.Controls.Add(Me.TextStudentID)
+        Me.Controls.Add(Me.TextFirstname)
+        Me.Controls.Add(Me.TextLastname)
+        Me.Controls.Add(Me.Guna2HtmlLabel2)
+        Me.Controls.Add(Me.Guna2HtmlLabel3)
+        Me.Controls.Add(Me.Guna2HtmlLabel4)
         Me.Name = "Test"
-        Me.Size = New System.Drawing.Size(397, 280)
+        Me.Size = New System.Drawing.Size(760, 407)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Guna2AnimateWindow1 As Guna.UI2.WinForms.Guna2AnimateWindow
+    Friend WithEvents Guna2HtmlLabel1 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2Separator1 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents TextStudentID As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents TextFirstname As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents TextLastname As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2HtmlLabel2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel3 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel4 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button2 As Guna.UI2.WinForms.Guna2Button
 
 End Class
