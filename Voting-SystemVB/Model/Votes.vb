6e8006624c57bef727df1266920c4f94cd7af021
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-11-30 15:06:11 +0800

    Add Votes Model

---
+++ Voting-SystemVB/Model/Votes.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data.OleDb
+
+Public Class Votes
+
+    Private Shared ReadOnly QUERY_COUNT_ALL = "SELECT COUNT(*) FROM (SELECT DISTINCT [student_id] FROM [Votes])"
+
+    Public Shared Async Function CountAllDone() As Task(Of Integer)
+        Dim Res As Integer = 0
+        Await GetConnection().OpenAsync()
+        Using Cmd As New OleDbCommand(QUERY_COUNT_ALL, GetConnection())
+            Res = Await Cmd.ExecuteScalarAsync()
+        End Using
+        GetConnection().Close()
+        Return Res
+    End Function
+
+End Class
