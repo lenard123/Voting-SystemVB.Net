54f602ca2592bbd8f8da5ac9bbcb40a280cd602c
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-12-06 14:26:08 +0800

    Add Comments on codes

--- Voting-SystemVB/AdminController/AdminHomeNotStarted.vb
+++ Voting-SystemVB/AdminController/AdminHomeNotStarted.vb
@@ -7,6 +7,7 @@
     Dim PartiesCount As Integer
     Dim CandidatesCounts As Dictionary(Of Integer, Integer)
 
+    'Create only one Instance for the whole Runtime
     Private Shared Instance As AdminHomeNotStarted
     Public Shared Function GetInstance() As AdminHomeNotStarted
         If IsNothing(Instance) Then
@@ -27,37 +28,11 @@
         Guna2WinProgressIndicator1.Stop()
     End Sub
 
-    Public Sub RefreshData() Implements MainControl.RefreshControl
-        StartLoading()
-        BackgroundWorkerFetchData.RunWorkerAsync()
-    End Sub
-
-    Private Sub BackgroundWorkerFetchData_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorkerFetchData.DoWork
-        VotersCount = Student.CountAll()
-        PreviousElectionCount = Election.CountAll() - 1
-        PartiesCount = Party.CountAll()
-        CandidatesCounts = New Dictionary(Of Integer, Integer)
-        For I As Integer = 1 To 6
-            CandidatesCounts.Add(I, Candidate.Count(I))
-        Next
-    End Sub
-
-    Private Sub BackgroundWorkerFetchData_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorkerFetchData.RunWorkerCompleted
-        StopLoading()
-        LabelVotersCount.Text = VotersCount
-        LabelParties.Text = PartiesCount
-        LabelPreviousElection.Text = PreviousElectionCount
-        LabelCandidates.Text = CandidatesCounts.Sum(Function(item As KeyValuePair(Of Integer, Integer))
-                                                        Return item.Value
-                                                    End Function)
-    End Sub
-
-    Private Sub ButtonStartElection_Click(sender As Object, e As EventArgs) Handles ButtonStartElection.Click
-        If CanStartElection() Then
-            StartElection.ShowPopup()
-        End If
-    End Sub
-
+    ''' <summary>
+    ''' To Check if the Positions have enough candidates to start the election
+    ''' </summary>
+    ''' <returns>Returns True if Every Candidates has atleast</returns>
+    ''' <remarks></remarks>
     Private Function CanStartElection() As Boolean
         For i = 1 To 6
             If CandidatesCounts(i) < 1 Then
@@ -69,5 +44,38 @@
         Next
         Return True
     End Function
+
+    'Trigger when the control is loaden on Admin Panel
+    Public Sub RefreshData() Implements MainControl.RefreshControl
+        StartLoading()
+        BackgroundWorkerFetchData.RunWorkerAsync()
+    End Sub
+
+    'Trigger when Start Button has click
+    Private Sub ButtonStartElection_Click(sender As Object, e As EventArgs) Handles ButtonStartElection.Click
+        If CanStartElection() Then
+            StartElection.ShowPopup()
+        End If
+    End Sub
+
+    'Background Workers - Fetching Data
+    Private Sub BackgroundWorkerFetchData_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorkerFetchData.DoWork
+        VotersCount = Student.CountAll()
+        PreviousElectionCount = Election.CountAll() - 1
+        PartiesCount = Party.CountAll()
+        CandidatesCounts = New Dictionary(Of Integer, Integer)
+        For I As Integer = 1 To 6
+            CandidatesCounts.Add(I, Candidate.Count(I))
+        Next
+    End Sub
+    Private Sub BackgroundWorkerFetchData_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorkerFetchData.RunWorkerCompleted
+        StopLoading()
+        LabelVotersCount.Text = VotersCount
+        LabelParties.Text = PartiesCount
+        LabelPreviousElection.Text = PreviousElectionCount
+        LabelCandidates.Text = CandidatesCounts.Sum(Function(item As KeyValuePair(Of Integer, Integer))
+                                                        Return item.Value
+                                                    End Function)
+    End Sub
 
 End Class
