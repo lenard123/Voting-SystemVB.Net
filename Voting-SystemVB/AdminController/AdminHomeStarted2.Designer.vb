fa6e813d8c4b41d8d34ad429774f5571526b3543
unknown <rustajada01@gmail.com>
2020-12-06 13:43:51 +0800

    Sarah x papalitan mona ata

--- Voting-SystemVB/AdminController/AdminHomeStarted2.Designer.vb
+++ Voting-SystemVB/AdminController/AdminHomeStarted2.Designer.vb
@@ -22,9 +22,9 @@ Partial Class AdminHomeStarted2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.President = New Guna.UI2.WinForms.Guna2Button()
         Me.VPresident = New Guna.UI2.WinForms.Guna2Button()
         Me.Secretary = New Guna.UI2.WinForms.Guna2Button()
@@ -254,16 +254,16 @@ Partial Class AdminHomeStarted2
         '
         'Chart1
         '
-        ChartArea1.Name = "ChartArea1"
-        Me.Chart1.ChartAreas.Add(ChartArea1)
-        Legend1.Name = "Legend1"
-        Me.Chart1.Legends.Add(Legend1)
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
         Me.Chart1.Location = New System.Drawing.Point(19, 12)
         Me.Chart1.Name = "Chart1"
-        Series1.ChartArea = "ChartArea1"
-        Series1.Legend = "Legend1"
-        Series1.Name = "Series1"
-        Me.Chart1.Series.Add(Series1)
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.Chart1.Series.Add(Series2)
         Me.Chart1.Size = New System.Drawing.Size(573, 144)
         Me.Chart1.TabIndex = 0
         Me.Chart1.Text = "Chart1"
@@ -280,6 +280,7 @@ Partial Class AdminHomeStarted2
         '
         Me.TotalVotersCircle.AccessibleName = "TotalVotersCircle"
         Me.TotalVotersCircle.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TotalVotersCircle.Animated = True
         Me.TotalVotersCircle.Controls.Add(Me.Label1)
         Me.TotalVotersCircle.Controls.Add(Me.TotalVotersCount)
         Me.TotalVotersCircle.FillThickness = 10
@@ -324,6 +325,7 @@ Partial Class AdminHomeStarted2
         '
         Me.AlreadyVotedCircle.AccessibleName = "AlreadyVotedCircle"
         Me.AlreadyVotedCircle.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.AlreadyVotedCircle.Animated = True
         Me.AlreadyVotedCircle.Controls.Add(Me.Label2)
         Me.AlreadyVotedCircle.Controls.Add(Me.AlreadyVotedCount)
         Me.AlreadyVotedCircle.FillThickness = 10
@@ -369,6 +371,7 @@ Partial Class AdminHomeStarted2
         '
         Me.RemainingVotersCircle.AccessibleName = "RemainingVotersCircle"
         Me.RemainingVotersCircle.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.RemainingVotersCircle.Animated = True
         Me.RemainingVotersCircle.Controls.Add(Me.Label4)
         Me.RemainingVotersCircle.Controls.Add(Me.RemainingVotersCount)
         Me.RemainingVotersCircle.FillThickness = 10
